4f23c4a53e9f8adcb5f2c9443ae83fd69b4b08e1
christopher-buss <christopher.buss+github@protonmail.com>
2023-07-13 15:11:48 +0100

    fix: incorrect gizmo types

--- src/gizmo.d.ts
+++ src/gizmo.d.ts
@@ -26,16 +26,16 @@ declare namespace Gizmo {
 	export function enable(): void;
 	export function disable(): void;
 
-	export const point: GizmoClass<(cframe: CFrame) => void>;
-	export const box: GizmoClass<(cframe: CFrame, size: Vector3) => void>;
-	export const wirebox: GizmoClass<(cframe: CFrame, size: Vector3) => void>;
-	export const sphere: GizmoClass<(cframe: CFrame, radius: number) => void>;
-	export const wiresphere: GizmoClass<(cframe: CFrame, radius: number) => void>;
-	export const line: GizmoClass<(start: Vector3, end: Vector3) => void>;
-	export const arrow: GizmoClass<(start: Vector3, end: Vector3) => void>;
-	export const ray: GizmoClass<(origin: Vector3, direction: Vector3) => void>;
+	export const point: GizmoClass<(position: Vector3) => void>;
+	export const box: GizmoClass<(orientation: CFrame, size: Vector3) => void>;
+	export const wirebox: GizmoClass<(orientation: CFrame, size: Vector3) => void>;
+	export const sphere: GizmoClass<(position: Vector3, radius: number) => void>;
+	export const wiresphere: GizmoClass<(position: Vector3, radius: number) => void>;
+	export const line: GizmoClass<(from: Vector3, to: Vector3) => void>;
+	export const arrow: GizmoClass<(from: Vector3, to: Vector3) => void>;
+	export const ray: GizmoClass<(from: Vector3, direction: Vector3) => void>;
 	export const plane: GizmoClass<(cframe: CFrame, size: Vector2 | undefined) => void>;
-	export const text: GizmoClass<(position: Vector3, text: string) => void>;
+	export const text: GizmoClass<(position: Vector3, text: string, ...args: Array<number | string>) => void>;
 }
 
 export = Gizmo;
